1117c0f4f8076ed8bb2e65e16e6124a1bcd75f9a
Brian Wagener <brianw@avalamarketing.com>
2014-05-24 13:25:54 -0500

    Added GetEventStore repository.

---
+++ src/proj/CommonDomain.Persistence.EventStore/AggregateDeletedException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CommonDomain.Persistence.EventStore
+{
+    public class AggregateDeletedException : Exception
+    {
+        public readonly Guid Id;
+        public readonly Type Type;
+
+        public AggregateDeletedException(Guid id, Type type)
+            : base(string.Format("Aggregate '{0}' (type {1}) was deleted.", id, type.Name))
+        {
+            Id = id;
+            Type = type;
+        }
+    }
+}
